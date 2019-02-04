84b6979fab194188fc041d950623660e9d46d3c8
Sujay Talukdar <sujaytalukder03@gmail.com>
2019-02-04 15:48:59 +0530

    Update random_slide.js

--- Random-slider_assignment/js/random_slide.js
+++ Random-slider_assignment/js/random_slide.js
@@ -1,37 +1,31 @@
 
   (function ($) {
-
     $.fn.slide = function (settings) {
 
         var config = $.extend({
-
-            delay: null, //3000
-
-            fadeSpeed: null //1000
-
+            delay: null
         }, settings);
+
         var slideIndex = 1;
-        const numImagesAvailable = 242; 
-
-        //const $slideshowContainer = document.querySelector('#slideshow-container');
-        let slideItem = document.createElement('div');
-
-        $.getJSON("https://picsum.photos/list", fetchData);
-
+        $.getJSON("https://picsum.photos/listed", fetchData, error); // fetch image details list call success callback   
+        
     function fetchData(data) {
-
         var arr = [], i;
         let randomImageIndex;
         var dispSlide;
 
         for(var i = 0; i< 10; i++ ){                           //to get random array list
 
-            randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+            randomImageIndex = Math.floor(Math.random() * data.length);
             arr.push(data[randomImageIndex]);
         }
 
-        localStorage.setItem("images", JSON.stringify(arr));
-        
+        if ( Modernizr.localstorage || typeof(Storage) !== "undefined" ) {
+
+           localStorage.setItem("images", JSON.stringify(arr));    //store random array list in a webstorage. 
+        } else {
+           console.log("Sorry! No Web Storage support.."); 
+        }              
         dispSlide = `
                     <div class="mySlides fade">                        
                         <img src="https://picsum.photos/200/300/?image=${arr[slideIndex -1]["id"]}" alt="">
@@ -40,18 +34,22 @@
         $(".slidecontent").html(dispSlide);  
     }
 
+    function error(jqXHR, textStatus, errorThrown) {
+
+        console.log("error " + textStatus);
+        console.log("incoming Text " + jqXHR.responseText);
+    } 
+
     function showSlides(n){ 
 
-        var slideArray = JSON.parse(localStorage.getItem("images") || "[]");   
-        var  i, slides;
-        var itemSlide;
+        var slideArray = JSON.parse(localStorage.getItem("images") || "[]"); // get 10 random array list from webstorage  
+        var slides , itemSlide;
         slides = slideArray.length;
     
-        if (n > slides) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides}
+        if ( n > slides ) { slideIndex = 1 }    
+        if ( n < 1 ) { slideIndex = slides }
 
-        /*$(".mySlides").hide(); *///hide current image
-        $(".mySlides").remove();
+        $(".mySlides").remove();  // remove existing div from dom
 
         itemSlide = `
                     <div class="mySlides fade">                        
@@ -60,11 +58,16 @@
                     </div>`
         $(".slidecontent").html(itemSlide);        
     }
+
     function plusSlides(n) {
         showSlides(slideIndex += n);
     }
 
-    setInterval(plusSlides, 3000, 1);
-    
+    setInterval(plusSlides, 3000, 1);   // call plusSlides on specific interval
+
+    return {                            // return function collection for use outside
+        showSlides: showSlides,  
+        plusSlides: plusSlides
+    }    
 } // end slide function
 } (jQuery));
