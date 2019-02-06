8b45daf993ccd2d8df7b7b5cd605abeee71f7b79
Sujay Talukdar <sujaytalukder03@gmail.com>
2019-02-06 11:00:39 +0530

    added comments in specific line of code.

--- Random-slider_assignment/js/random_slide.js
+++ Random-slider_assignment/js/random_slide.js
@@ -1,32 +1,30 @@
 
   (function ($) {
     $.fn.slide = function (settings) {
-
-        var config = $.extend({
+        //here settings object holds the values which have been passed as a parameter
+        //in the slide function.
+        var config = $.extend({   //extends function replace the default values to the passed values.
             delay: null,
             items: null
-        }, settings);
+        }, settings);  
 
         var slideIndex, arr;
-            slideIndex = 1;
+            slideIndex = 1;   // initial slideindex value.
             arr = [];
-        getData();    
+        getData();    // it is calling on call slide.
       
-    function getData(){
-
-        var baseUrl = "https://picsum.photos/list";
-        $.getJSON(baseUrl , initSlide, error);
-
-    }  // fetch image details list call success callback   
+    function getData() {  // fetch image details list and call success and error callback.
+        var baseUrl = "https://picsum.photos/list";  // this is the base url for fetch image details list.
+        $.getJSON(baseUrl , initSlide, error);  
+    }   
         
-    function initSlide(data) {
+    function initSlide(data) {              // initSlide() is a callback function calling for initial slide.
         var  i, dispSlide;
         let randomImageIndex;
 
-        for(var i = 0; i< config.items; i++ ){                           //to get random array list
-
-            randomImageIndex = Math.floor(Math.random() * data.length);
-            arr.push(data[randomImageIndex]);
+        for( i = 0; i< config.items; i++ ){                           //to get random array list.
+            randomImageIndex = Math.floor(Math.random() * data.length); // to get random index from data length.
+            arr.push(data[randomImageIndex]);                           // push random list in an array.
         }
         
         dispSlide = `
@@ -34,20 +32,18 @@
                         <img src="https://picsum.photos/200/300/?image=${arr[slideIndex -1]["id"]}" alt="">
                         <div class="text">${arr[slideIndex -1]["author"]}</div>
                     </div>`
-        $(".slidecontent").html(dispSlide);
+        $(".slidecontent").html(dispSlide); 
     }
 
-    function error(jqXHR, textStatus, errorThrown) {
-
+    function error(jqXHR, textStatus, errorThrown) {  // this callback is for if ajax is fail.
         console.log("error " + textStatus);
         console.log("incoming Text " + jqXHR.responseText);
     } 
 
-    function showSlides(n){ 
-
+    function showSlides(n) {           // this function is call for next slide.
        var slides , itemSlide, slideArray;        
-        slideArray = arr;
-        slides = slideArray.length;
+        slideArray = arr;                // assign random array list.
+        slides = slideArray.length;      // assign how many slide we want to show.
     
         if ( n > slides ) { slideIndex = 1 }    
         if ( n < 1 ) { slideIndex = slides }
@@ -60,13 +56,13 @@
         $(".slidecontent").html(itemSlide);        
     }
 
-    function plusSlides(n) {
+    function plusSlides(n) {   // plusSlides() is call for pass initial value.
         showSlides(slideIndex += n);
     }
 
     setInterval(plusSlides, config.delay, 1);   // call plusSlides on specific interval
 
-    return {                            // return function collection for use outside
+    return {                            // return collection of methods for use outside.
         showSlides: showSlides,  
         plusSlides: plusSlides
     }    
